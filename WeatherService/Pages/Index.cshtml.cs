f02318d43dea3a6d48516ce00196f04ee4d39070
ingimarlg <ingimarlg@advania.is>
2023-05-20 22:26:22 +0000

    Added Home page

--- WeatherService/Pages/Index.cshtml.cs
+++ WeatherService/Pages/Index.cshtml.cs
@@ -1,20 +1,78 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WeatherService.API.Clients;
+using WeatherService.API.Models;
 
-namespace WeatherService.Pages
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly IWeatherServiceClient _weatherServiceClient;
+    private readonly ILogger<IndexModel> _logger;
+    private const string _DATEFORMAT = "yyyy-MM-dd";
+
+    public IndexModel(ILogger<IndexModel> logger, IWeatherServiceClient weatherServiceClient)
     {
-        private readonly ILogger<IndexModel> _logger;
+        _logger = logger;
+        _weatherServiceClient = weatherServiceClient;
+    }
 
-        public IndexModel(ILogger<IndexModel> logger)
+    public WeatherModel? CurrentWeatherData { get; set; }
+    public WeatherModel? ForecastData { get; set; }
+    public WeatherModel? HistoricalData { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+    public string City { get; set; } = string.Empty;
+
+    [BindProperty(SupportsGet = true)]
+    public DateTime? Date { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+    public int? ForecastDays { get; set; } = 3;
+
+    [BindProperty]
+    public string FetchType { get; set; } = string.Empty;
+
+    public IActionResult OnGet()
+    {
+        return Page();
+    }
+
+    public async Task OnPostAsync()
+    {
+        if (FetchType == "Current")
         {
-            _logger = logger;
+            try
+            {
+                CurrentWeatherData = await _weatherServiceClient.FetchCurrentWeather(City);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exception when fetching current data");
+            }
         }
-
-        public void OnGet()
+        else if (FetchType == "Forecast")
         {
-
+            try
+            {
+                ForecastData = await _weatherServiceClient.FetchForecast(City, ForecastDays);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exception when fetching forecast data");
+            }
+        }
+        else if (FetchType == "History")
+        {
+            try
+            {
+                if (Date.HasValue)
+                {
+                    HistoricalData = await _weatherServiceClient.FetchHistoricalWeather(City, Date.Value.ToString(_DATEFORMAT));
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exception when fetching historical data");
+            }
         }
     }
 }
