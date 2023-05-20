a68b65414d67eef4e2765ecb5eee492290aae6ec
ingimarlg <ingimarlg@advania.is>
2023-05-20 12:35:45 +0000

    blind retry 8

--- WeatherService.API/Program.cs
+++ WeatherService.API/Program.cs
@@ -9,10 +9,11 @@ builder.Services.AddWeatherServices(builder.Configuration);
 builder.Services.AddControllers();
 
 // Add Endpoints API Explorer service which is required for the Swagger
-builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddEndpointsApiExplorer();
 
 // Add Swagger services
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+
 //builder.Services.AddSwaggerGen(c =>
 //{
 //    c.SwaggerDoc("v1", new() { Title = "WeatherService.API", Version = "v1" });
@@ -21,21 +22,23 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Enable middleware to serve generated Swagger as a JSON endpoint
-app.UseSwagger();
+//app.UseSwagger();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
+    //app.UseExceptionHandler("/Home/Error");
+    //app.UseHsts();
 
     // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
     // specifying the Swagger JSON endpoint
-    app.UseSwaggerUI();
+    //app.UseSwaggerUI();
     //app.UseSwaggerUI(c =>
     //{
     //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "WeatherService.API v1");
     //    //c.RoutePrefix = string.Empty;
     //});
-}
+//}
 
 app.UseHttpsRedirection();
 
