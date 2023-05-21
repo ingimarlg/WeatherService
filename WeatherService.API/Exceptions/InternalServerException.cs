ba9c2e8f230807ef25da0d219d98b70d186625ef
ingimarlg <ingimarlg@advania.is>
2023-05-21 16:05:46 +0000

    Add exception classes

---
+++ WeatherService.API/Exceptions/InternalServerException.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherService.API.Exceptions;
+
+public class InternalServerException : Exception
+{
+    public InternalServerException() { }
+
+    public InternalServerException(string message) : base(message) { }
+
+    public InternalServerException(string message, Exception innerException) : base(message, innerException) { }
+}
---
+++ WeatherService.API/Exceptions/WeatherApiException.cs
@@ -0,0 +1,18 @@
+﻿using WeatherService.API.Models;
+
+namespace WeatherService.API.Exceptions;
+
+public class WeatherApiException : Exception
+{
+    public WeatherApiErrorDetails ErrorDetails { get; set; } = new();
+
+    public WeatherApiException() { }
+
+    public WeatherApiException(string message) : base(message) { }
+
+    public WeatherApiException(WeatherApiErrorDetails errorDetails, string message)
+        : base(message)
+    {
+        ErrorDetails = errorDetails;
+    }
+}
